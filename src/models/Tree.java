296847e5fdb6db3ac90ed9733dd7fc6791919e3a
DanielGaona2798 <daniel.gaona01@uptc.edu.co>
2018-04-06 16:54:23 -0500

    añadir clase tree

---
+++ src/models/Tree.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Tree {
+
+	private Node root;
+
+	public void add(Node node) {
+		if (root != null) {
+			add(root, node);
+		}else {
+			root = node;
+		}
+	}
+
+	
+}
